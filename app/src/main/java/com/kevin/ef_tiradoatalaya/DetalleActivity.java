4bca3c6f1600cc1d73a5f2d71f006066701d575d
kevindevperu <tiradoatalaya@gmail.com>
2023-08-24 01:21:16 -0500

    Nueva Actualizacion

--- app/src/main/java/com/kevin/ef_tiradoatalaya/DetalleActivity.java
+++ app/src/main/java/com/kevin/ef_tiradoatalaya/DetalleActivity.java
@@ -1,14 +1,34 @@
 package com.kevin.ef_tiradoatalaya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.kevin.ef_tiradoatalaya.data.fragments.HomeFragment;
+import com.kevin.ef_tiradoatalaya.databinding.ActivityDetalleBinding;
 
 public class DetalleActivity extends AppCompatActivity {
+
+    private ActivityDetalleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle);
+        binding = ActivityDetalleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Toolbar toolbar = findViewById(R.id.tb_detalle);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Detalles del Titan");
+        addHomeFragment2();
     }
+
+    private void addHomeFragment2(){
+        getSupportFragmentManager().
+                beginTransaction()
+                .add(binding.listTitanDetalle.getId(), new HomeFragment()).commit();
+    }
+
 }
--- app/src/main/java/com/kevin/ef_tiradoatalaya/data/model/Titan.java
+++ app/src/main/java/com/kevin/ef_tiradoatalaya/data/model/Titan.java
@@ -3,14 +3,31 @@ package com.kevin.ef_tiradoatalaya.data.model;
 import java.util.List;
 
 public class Titan {
+
+    private  int id;
     private String name;
     private String img;
     private List<String> abilities;
 
-    public Titan(String name, String img, List<String> abilities) {
+    private String height;
+
+    private String allegiance;
+
+    public Titan(int id, String name, String img, List<String> abilities, String height, String allegiance) {
+        this.id = id;
         this.name = name;
         this.img = img;
         this.abilities = abilities;
+        this.height = height;
+        this.allegiance = allegiance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -35,5 +52,21 @@ public class Titan {
 
     public void setAbilities(List<String> abilities) {
         this.abilities = abilities;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getAllegiance() {
+        return allegiance;
+    }
+
+    public void setAllegiance(String allegiance) {
+        this.allegiance = allegiance;
     }
 }
